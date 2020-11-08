116004c123788b4200ce09cf5285c08dff0a1939
Alan Hanson <alan@A-CM-MBP.local>
2020-11-07 22:47:19 -0800

    Add CLI functionality and ability to delete.
    Made the program function through the CLI loop.
    Added delete to commands.
    Improved show command output.
    Added CLI help.
    
    Made into functions put/get/show commands.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate serde_derive;
 use process::exit;
 use restson::{Error, RestClient, RestPath};
 use std::collections::HashMap;
+use std::io;
+use std::io::Write;
 use std::{env, process};
 
 const CM_URL: &'static str = "CRYPTOMOVE_URL";
@@ -69,23 +71,36 @@ impl RestPath<()> for Show<'_> {
 
 // Put secrets uses these
 #[derive(Serialize, Debug)]
-struct Put {
-    email: String,
-    key_name: String,
+struct Put<'a> {
+    email: &'a String,
+    key_name: &'a str,
     key_value: String,
 }
 
-impl RestPath<()> for Put {
+impl RestPath<()> for Put<'_> {
     fn get_path(_: ()) -> Result<String, Error> {
         Ok(String::from("api/v1/user/secret/protect"))
     }
 }
 
+// Delete secrets uses these
+#[derive(Serialize, Debug)]
+struct Delete<'a> {
+    email: &'a String,
+    key_name: &'a str,
+}
+
+impl RestPath<()> for Delete<'_> {
+    fn get_path(_: ()) -> Result<String, Error> {
+        Ok(String::from("api/v1/user/secret/delete"))
+    }
+}
+
 // Get secrets uses these
 #[derive(Serialize, Debug)]
-struct Get {
-    email: String,
-    key_name: String,
+struct Get<'a> {
+    email: &'a String,
+    key_name: &'a str,
 }
 
 #[derive(Debug, serde_derive::Deserialize)]
@@ -95,9 +110,59 @@ struct GetResult {
     status: String,
 }
 
-impl RestPath<()> for Get {
+impl RestPath<()> for Get<'_> {
     fn get_path(_: ()) -> Result<String, Error> {
         Ok(String::from("api/v1/user/secret/expose"))
+    }
+}
+
+fn put_key(client: &mut RestClient, username: &String, key: &str) {
+    let data = Put {
+        email: username,
+        key_name: key,
+        key_value: String::from("tsur"),
+    };
+    let result = client.post((), &data);
+    match result {
+        Ok(_) => {
+            println!("Put secret at name {:?}", key);
+        }
+        Err(error) => {
+            println!("Error with put for secret {}\n{}", key, error);
+        }
+    }
+}
+
+fn delete_key(client: &mut RestClient, username: &String, key: &str) {
+    let data = Delete {
+        email: username,
+        key_name: key,
+    };
+    let result = client.post((), &data);
+    match result {
+        Ok(_) => {
+            println!("Deleted {:?}", key);
+        }
+        Err(error) => {
+            println!("Error deleting secret {}\n{}", key, error);
+        }
+    }
+}
+
+fn get_key(client: &mut RestClient, username: &String, key: &str) {
+    let data = Get {
+        email: username,
+        key_name: key,
+    };
+    // let res = client.post_capture((), &data).context("Failed to find secret")?;
+    let result: std::result::Result<GetResult, restson::Error> = client.post_capture((), &data);
+    match result {
+        Ok(success) => {
+            println!("{:?}", success.key_value);
+        }
+        Err(error) => {
+            println!("Could not find secret {}\n{}", key, error);
+        }
     }
 }
 
@@ -105,9 +170,23 @@ fn show_keys(client: &mut RestClient, username: &String) {
     let data = Show { email: username };
     let res: ShowKeys = client.post_capture((), &data).unwrap();
 
-    println!("{:25} {:5} {}", "Date Created", "Link", "Name");
+    // Default to the column header size
+    let mut max = 5;
+    for (key, _) in &res.keys {
+        if key.len() > max {
+            max = key.len();
+        }
+    }
+
+    println!("{0:1$} {2:25} {3}", "Name", max, "Date Created", "Link");
+    let header_width = max + 1 + 25 + 1 + 5;
+    println!("{0:->1$}", "-", header_width);
+
     for (key, value) in res.keys {
-        println!("{} {:5} {}", value.last_saved_time, value.is_link, key);
+        println!(
+            "{0:1$} {2:5} {3}",
+            key, max, value.last_saved_time, value.is_link
+        );
     }
 }
 
@@ -146,6 +225,88 @@ fn read_env(env_name: &str) -> String {
     };
     env_val
 }
+
+// from: https://stackoverflow.com/questions/38447780
+fn crop_letters(s: &str, pos: usize) -> &str {
+    match s.char_indices().skip(pos).next() {
+        Some((pos, _)) => &s[pos..],
+        None => "",
+    }
+}
+
+fn show_help() {
+    println!("Valid commands are:");
+    println!("-------------------------------------------------------------------");
+    println!("show                 | Show all secrets");
+    println!("get <SECRET_NAME>    | Get the value of the secret name provided");
+    println!("put <SECRET_NAME>    | Put a secret at the name provided,");
+    println!("                     | you will be asked for the secrets contents");
+    println!("delete <SECRET_NAME> | Delete the value of the secret name provided");
+    println!("q | quit             | Quit the program");
+}
+
+fn cli(client: &mut RestClient, username: &String) {
+    show_help();
+    loop {
+        print!("cmcli> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let line = line.trim();
+
+        let cmd = match line.split_ascii_whitespace().next() {
+            Some(val) => val,
+            None => {
+                println!("Enter a command, q for quit");
+                continue;
+            }
+        };
+
+        // First comes the command, if there is anythign else, it's the
+        // name of a secret to get, put, or delete.  We want whatever comes
+        // after the command as a whole string including any spaces.
+        let cmdl = cmd.len();
+        let args = crop_letters(&line, cmdl).trim();
+
+        match cmd {
+            "show" => {
+                show_keys(client, &username);
+            }
+            "get" => {
+                if args.len() == 0 {
+                    println!("Missing secret name to get");
+                    continue;
+                };
+                get_key(client, &username, &args);
+            }
+            "put" => {
+                if args.len() == 0 {
+                    println!("Missing secret name to put");
+                    continue;
+                };
+                println!("go put {:?}", args);
+                put_key(client, &username, &args);
+            }
+            "delete" => {
+                if args.len() == 0 {
+                    println!("Missing secret name to delete");
+                    continue;
+                };
+                delete_key(client, &username, &args);
+            }
+            "q" | "quit" => {
+                println!("quit");
+                break;
+            }
+            _ => {
+                println!("Unknown command {:?}", cmd);
+                show_help();
+            }
+        };
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cm_url = read_env(CM_URL);
     let cm_username = read_env(CM_USER);
@@ -158,28 +319,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = get_token(&cm_url, &cm_username, &cm_password)?;
 
-    show_keys(&mut client, &cm_username);
-    /*
-        let data = Put {
-            email: String::from("cmtest002@cryptomove.com"),
-            key_name: String::from("rust"),
-            key_value: String::from("tsur"),
-        };
-        let res = client.post((), &data).expect("Failed to put secret");
-        println!("{:?}", res);
-    */
-    let data = Get {
-        email: cm_username,
-        key_name: String::from("rust"),
-    };
-    // let res = client.post_capture((), &data).context("Failed to find secret")?;
-    let result: std::result::Result<GetResult, restson::Error> = client.post_capture((), &data);
-    match result {
-        Ok(success) => println!("Success {:?}", success),
-        Err(error) => {
-            println!("Could not find secret rust:\n{}", error);
-        }
-    }
+    cli(&mut client, &cm_username);
 
     Ok(())
 }
